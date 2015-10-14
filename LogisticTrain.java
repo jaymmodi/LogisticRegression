7fe113bb7934156ebdb98a41398076f898e90ab9
Jay Modi <modi.jay6@gmail.com>
2015-10-13 21:40:55 -0400

    optimizing the parse File Logic

---
+++ LogisticTrain.java
@@ -0,0 +1,10 @@
+package LogisticRegression;
+
+import java.util.ArrayList;
+
+public class LogisticTrain {
+
+    public void train(ArrayList<TrainingExample> dataSet) {
+
+    }
+}
--- ParseFile.java
+++ ParseFile.java
@@ -8,45 +8,54 @@ public class ParseFile {
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<TrainingExample> examples = new ArrayList<TrainingExample>();
         try {
             String trainingSetPath = br.readLine();
-            FileReader fileReader = new FileReader(new File(trainingSetPath));
+            br.close();
 
-            br = new BufferedReader(fileReader);
-            String line = null;
-
-            int index = 1;
             int classifyLabel = 1;
 
-            while ((line = br.readLine()) != null) {
-                String perLine[] = line.split(",");
+            ArrayList<TrainingExample> dataSet = createDataSet(classifyLabel, trainingSetPath);
 
-                TrainingExample trainingExample = new TrainingExample();
-                trainingExample.setIndex(index);
-                int[] values = new int[perLine.length - 1];
-
-                for (int i = 0; i < perLine.length; i++) {
-                    if (i == perLine.length - 1) {
-                        if ((Integer.parseInt(perLine[i]) != classifyLabel)) {
-                            trainingExample.setLabel(0);
-                        } else {
-                            trainingExample.setLabel(classifyLabel);
-                        }
-                    } else {
-                        values[i] = Integer.parseInt(perLine[i]);
-                    }
-                }
-                index++;
-                trainingExample.setValues(values);
-                examples.add(trainingExample);
-            }
+            System.out.println(dataSet.size());
+            LogisticTrain logisticTrain = new LogisticTrain();
+            logisticTrain.train(dataSet);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println(examples.size());
-
     }
+
+    public static ArrayList<TrainingExample> createDataSet(int classifyLabel, String fileName) throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String line = null;
+        int index = 1;
+        ArrayList<TrainingExample> examples = new ArrayList<TrainingExample>();
+
+        while ((line = br.readLine()) != null) {
+            String perLine[] = line.split(",");
+
+            TrainingExample trainingExample = new TrainingExample();
+            trainingExample.setIndex(index);
+            int[] values = new int[perLine.length - 1];
+
+            for (int i = 0; i < perLine.length; i++) {
+                if (i == perLine.length - 1) {
+                    if ((Integer.parseInt(perLine[i]) != classifyLabel)) {
+                        trainingExample.setLabel(0);
+                    } else {
+                        trainingExample.setLabel(classifyLabel);
+                    }
+                } else {
+                    values[i] = Integer.parseInt(perLine[i]);
+                }
+            }
+            index++;
+            trainingExample.setValues(values);
+            examples.add(trainingExample);
+        }
+        br.close();
+        return examples;
+    }
+
 }
